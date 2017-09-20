f07e66c38d24c37a16297ca93becf592a65cf361
Steff <srkob1@student.monash.edu>
2017-09-20 11:54:20 +1000

    Begun Preview Page
    theme, folder and image buttons

--- R/taipan.R
+++ R/taipan.R
@@ -11,46 +11,77 @@ dplyr::`%>%`
 library(shiny)
 library(shinythemes)
 
-ui <-navbarPage(theme = shinytheme("spacelab"), "QuestionInput",
+ui <- navbarPage(theme = shinytheme("spacelab"), "QuestionInput",
                 tabPanel("Edit", icon = icon("pencil"),
                          fluidRow(
-                           column(6, textInput("dir","Directory","Path")),
-                           column(6, actionButton("dirin", "Load Folder"))),
+                           column(8, textInput("dir","Directory","Path")),
+                           column(2, offset = 2, actionButton("dirin", "Load Folder"))),
 
                          #fluidRow(plotOutput("plotE")), add preview when folder path is selected?
                          wellPanel(
                            fluidRow(
                              tabsetPanel(
                                tabPanel("Scene",
-                                        wellPanel(id = "qs", style = "overflow-y:scroll; max-height: 600px",
-                                                  fluidRow(column(1),
-                                                           column(11,textInput("q","New Question","Question text"),
+                                        wellPanel(id = "qs", style = "overflow-y:scroll; max-height: 600px;",
+                                                  fluidRow(column(2, offset=1, actionButton("addq", "Add a Question", icon("plus")))),
+                                                  fluidRow(column(11, offset = 1 ,textInput("q","New Question","Question text"),
                                                                   selectInput("qtype", "Question type", c("Check box" = "check",
                                                                                                           "Radio button" = "radio")),
-                                                                  fluidRow(column(9,textInput("a","Answers", "Enter possible choice")),
-                                                                           column(3, actionButton("adda",icon("plus")),
-                                                                                  actionButton("removea",icon("times"))))),
-                                                           fluidRow(column(11),
-                                                                    column(1, actionButton("addq", icon("plus"))))
-                                                  ))),
+                                                                  fluidRow(column(10,textInput("a",NULL, "Enter possible choice")),
+                                                                           column(2,
+                                                                                 # div(style= "position: relative;",
+                                                                                #  div(style= "position: absolute; bottom: 0",
+                                                                             actionButton("adda",icon("plus")),
+                                                                             actionButton("removea",icon("times")), style= "bottom:0;"
+                                                                                        # ))
+                                                                                  ))
+
+                                                  )))),
 
                                tabPanel("Selection",
                                         wellPanel(
                                           fluidRow("Add Question section when confirmed")))
-                             )
-                           ),
+
+                           ))
+                         ),
 
                            fluidRow(column(1),
-                                    column(9, actionButton("back",icon("arrow-left"))),
-                                    column(2, actionButton("save",icon("check")),
-                                           actionButton("remove",icon("arrow-right")))))),
+                                    column(3, actionButton("folderl", "Previous Folder", icon("arrow-left"))),
+                                    column(4, offset=4, actionButton("save", "Save Questions", icon("check")),
+                                              actionButton("folderr","Next Folder",icon("arrow-right"))))
+
+                           ),
+
 
                 tabPanel("Preview", icon = icon("search"),
+
                          tabPanel("Plot", plotOutput("plotP")),
                          wellPanel(
+                           fluidRow(
+                             tabsetPanel(
+                               tabPanel("Scene",
+                                        wellPanel(
+                                          fluidRow("Questions regarding the entire image")
+                                          )),
+
+                               tabPanel("Selection",
+                                        wellPanel(
+                                          fluidRow("Questions regarding a selected area"
+                                                   )
+                                          ))
+                             ))),
+
+                         fluidRow(column(1),
+                                  column(6, actionButton("imagel", "Previous Image", icon("arrow-left"))),
+                                  column(5, actionButton("save", "Save Answers", icon("check")),
+                                         actionButton("imager","Next Image",icon("arrow-right")))),
+
+                         fluidRow(column(1),
+                                    column(6, actionButton("folderl", "Previous Folder", icon("arrow-left"))),
+                                    column(5, actionButton("save", "Save Answers", icon("check")),
+                                           actionButton("folderr","Next Folder",icon("arrow-right"))))
                          )
 
-                )
 )
 
 
