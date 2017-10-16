4743f696b46abf6a4017c21a8b7c57273ba128bd
Stephanie Kobakian <srkob1@student.monash.edu>
2017-10-16 16:00:51 +1100

    Change height of image and push question section down to accomodate

--- R/taipan.R
+++ R/taipan.R
@@ -14,20 +14,27 @@ library(shinyWidgets)
 # not necessary if importing pipe function
 library(tidyverse)
 library(tools) #file extension
+library(imager)
+
+
 
 ui <- navbarPage(theme = shinytheme("spacelab"), "QuestionInput",
                 tabPanel("Edit", icon = icon("pencil"),
-                         fluidRow(
-                           column(3, "File Path"),
-                           column(5, offset = 2, selectInput("folderSelect", "Select Folder Path", choices = img_folders))),
 
-                         actionButton("debug", "debug"),
+                         # allow for selection between folders
+                         #fluidRow(column(3, "File Path"),
+                         #  column(5, offset = 2, selectInput("folderSelect", "Select Folder Path", choices = img_folders))),
+
+                        actionButton("debug", "debug"),
 
 
                          #conditional panel
-                         #fluidRow(plotOutput("plotE")), add preview when folder path is selected?
+                        #conditionalPanel(length(imglist)>0,
+                          #               div(id = "plotE", )),
 
-                         wellPanel(
+
+                        uiOutput("plotUI"),
+                        wellPanel(
                            fluidRow(
                              tabsetPanel(
                                tabPanel("Scene",
@@ -40,15 +47,13 @@ ui <- navbarPage(theme = shinytheme("spacelab"), "QuestionInput",
                                                   fluidRow(column(2, offset=1, actionButton("selection_addq", "Add a Question", icon("plus")))),
                                                   div(id = "selection_questions")))
 
-                           ))
-                         ),
+                           ))),
 
                            fluidRow(column(1),
                                     column(3, actionButton("folderl", "Previous Folder", icon("arrow-left"))),
                                     column(4, offset=4, downloadButton("saveq", "Save Questions", icon("check")),
                                               actionButton("folderr","Next Folder",icon("arrow-right"))))
-
-                           ),
+                        ),
 
 
                 tabPanel("Preview", icon = icon("search"),
@@ -79,61 +84,51 @@ ui <- navbarPage(theme = shinytheme("spacelab"), "QuestionInput",
                                     column(5, actionButton("savef", "Save Answers", icon("check")),
                                            actionButton("folderr","Next Folder",icon("arrow-right"))))
                          )
-
-)
-
+                )
 
 
-server <-function(input, output, session) {
-  source("modules.R") # Temporary - encorporate into the package
-  source("helpers.R") # Temporary - encorporate into the package
-
-  #if there are no images in the folder in the folder path
-  # stop and give a message to reset folder path
-# run from beginning
-  if("images" %in% list.files()){
-    img_folders <- c()
-    for (path in list.dirs("images")) {
-      exts <- c("jpg", "png")
-      if (length(list_files_with_exts(file.path(path), exts))>0){
-        img_folders <- c(img_folders, path)
-      }
-    }
-  }
-
-
-  observeEvent(input$folderSelect, {
-    currentFilePath <- input$folderSelect
-    #populate questions with previous question data
-
-  })
-
+server <- function(input, output, session) {
+  source("modules.R")
+  source("helpers.R")
+  source("global.R")
 
   #debug
   observeEvent(input$debug, {
     browser()
   })
 
-  v <- try(reactiveValues(imgcount = dir(),
-                          questions = read.csv("questions.csv"),
-                          qdata = rep(NA,6))
-           )
-
-  if(class(v)=="try-error"){
-    message("Previous question set not found")
-  }
 
   v2 <- reactiveValues(img_questions = list(selection = list(),
-                                            scene = list())
-                       )
+                                            scene = list()))
+
+  height <- function() {
+    session$clientData$output_plotE_width*(450/800)
+  }
+
+
+  output$plotUI <- renderUI({
+    plotOutput(
+      "plotE",
+      #write a function to automatically find these depending on p ratio of the chosen image
+      width = "80vw", height = "56vw"
+    )
+  })
 
   output$plotE <- renderPlot({
-    plot(cars, type=input$plotType)
+    plot(load.image(imglist[1]))}, width="auto"
+  )
+
+  output$imgInfo <- renderText({
+    paste0("Image: ", " (", imglist[1], ")")
   })
 
   output$plotP <- renderPlot({
-    plot(cars, type=input$plotType)
-  })
+    plot(load.image(imglist[1]))},
+    height = function() {
+      session$clientData$output_plotP_width*(450/800)
+    }
+  )
+
 
   observeEvent(input$scene_addq, {
     insertUI("#scene_questions", ui = questionInputUI(paste0("question", input$scene_addq)))
@@ -158,9 +153,8 @@ server <-function(input, output, session) {
     contentType = "text/csv"
   )
 
-
-
 }
+
 
 
 shinyApp(ui, server)
