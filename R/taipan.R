65ec59b05e201afe26f231228cae1512fdd74d48
Stephanie Kobakian <srkob1@student.monash.edu>
2017-10-28 02:04:08 +1100

    Preview and Edit plots

--- R/taipan.R
+++ R/taipan.R
@@ -33,44 +33,42 @@ ui <- navbarPage(theme = shinytheme("spacelab"), "QuestionInput",
                           #               div(id = "plotE", )),
 
 
-                        uiOutput("plotUI"),
+                        uiOutput("plotUIE"),
+                        verbatimTextOutput("info"),
+
                         wellPanel(
                            fluidRow(
-                             tabsetPanel(
-                               tabPanel("Scene",
+                             tabsetPanel(id="areaQuestionsE", selected = "SceneE",
+                               tabPanel(title ="Scene", value = "SceneE",
                                         wellPanel(style = "overflow-y:scroll; max-height: 600px;",
                                                   fluidRow(column(2, offset=1, actionButton("scene_addq", "Add a Question", icon("plus")))),
                                                   div(id = "scene_questions"))),
 
-                               tabPanel("Selection",
+                               tabPanel(title = "Selection", value = "SelectionE",
                                         wellPanel(style = "overflow-y:scroll; max-height: 600px;",
                                                   fluidRow(column(2, offset=1, actionButton("selection_addq", "Add a Question", icon("plus")))),
                                                   div(id = "selection_questions")))
 
                            ))),
 
-                           fluidRow(column(1),
-                                    column(3, actionButton("folderl", "Previous Folder", icon("arrow-left"))),
-                                    column(4, offset=4, downloadButton("saveq", "Save Questions", icon("check")),
-                                              actionButton("folderr","Next Folder",icon("arrow-right"))))
+                           fluidRow(column(2, offset=10,downloadButton("saveq", "Save Questions", icon("check"))))
                         ),
 
 
                 tabPanel("Preview", icon = icon("search"),
 
-                         tabPanel("Plot", plotOutput("plotP")),
+                         tabPanel("Plot", uiOutput("plotUIP")),
                          wellPanel(
                            fluidRow(
-                             tabsetPanel(
-                               tabPanel("Scene",
+                             tabsetPanel(id="areaQuestionsP", selected = "SceneP",
+                               tabPanel("Scene", value = "SceneP",
                                         wellPanel(
                                           fluidRow("Questions regarding the entire image")
                                           )),
 
-                               tabPanel("Selection",
+                               tabPanel("Selection", value = "SelectionP",
                                         wellPanel(
-                                          fluidRow("Questions regarding a selected area"
-                                                   )
+                                          fluidRow("Questions regarding a selected area")
                                           ))
                              ))),
 
@@ -79,10 +77,7 @@ ui <- navbarPage(theme = shinytheme("spacelab"), "QuestionInput",
                                   column(5, actionButton("savei", "Save Answers", icon("check")),
                                          actionButton("imager","Next Image",icon("arrow-right")))),
 
-                         fluidRow(column(1),
-                                    column(6, actionButton("folderl", "Previous Folder", icon("arrow-left"))),
-                                    column(5, actionButton("savef", "Save Answers", icon("check")),
-                                           actionButton("folderr","Next Folder",icon("arrow-right"))))
+                         fluidRow(column(2, offset=10, actionButton("savef", "Save Answers", icon("check"))))
                          )
                 )
 
@@ -101,32 +96,65 @@ server <- function(input, output, session) {
   v2 <- reactiveValues(img_questions = list(selection = list(),
                                             scene = list()))
 
-  height <- function() {
-    session$clientData$output_plotE_width*(450/800)
-  }
 
-
-  output$plotUI <- renderUI({
+  output$plotUIE <- renderUI({
     plotOutput(
       "plotE",
+      click = "plotE_click",
+      dblclick = "plotE_dblclick",
+      hover = "plotE_hover",
+      brush = brushOpts(
+        id = "plotE_brush",
+          resetOnNew = TRUE),
       #write a function to automatically find these depending on p ratio of the chosen image
-      width = "80vw", height = "56vw"
+      width = "80vw", height = paste0(round((hwratio*100),0), "vw")
     )
   })
 
   output$plotE <- renderPlot({
-    plot(load.image(imglist[1]))}, width="auto"
+    plot(img1)}, width="auto"
   )
 
+  # add title above plot
   output$imgInfo <- renderText({
     paste0("Image: ", " (", imglist[1], ")")
   })
 
+ # switch between question sets if an area is selected by the brush
+  observeEvent(input$plotE_brush, {
+    if (is.null(input$plotE_brush)){
+      updateTabsetPanel(session, "areaQuestionsE",
+                        selected = "SceneE")}
+    else if (!is.null(input$plotE_brush)){
+      updateTabsetPanel(session, "areaQuestionsE",
+                        selected = "SelectionE")}
+  })
+
+  # switch between question sets if an area is selected by the brush
+  observeEvent(input$plotP_brush, {
+    if (is.null(input$plotP_brush)){
+      updateTabsetPanel(session, "areaQuestionsP",
+                        selected = "SceneP")}
+    else if (!is.null(input$plotE_brush)){
+      updateTabsetPanel(session, "areaQuestionsP",
+                        selected = "SelectionP")}
+  })
+
+
+    output$plotUIP <- renderUI({
+    plotOutput(
+      "plotP",
+      click = "plotP_click",
+      dblclick = "plotP_dblclick",
+      hover = "plotP_hover",
+      brush = "plotP_brush",
+      #write a function to automatically find these depending on p ratio of the chosen image
+      width = "80vw", height = paste0(round((hwratio*100),0), "vw")
+    )
+  })
+
   output$plotP <- renderPlot({
-    plot(load.image(imglist[1]))},
-    height = function() {
-      session$clientData$output_plotP_width*(450/800)
-    }
+    plot(img1)}, width="auto"
   )
 
 
@@ -152,6 +180,26 @@ server <- function(input, output, session) {
     },
     contentType = "text/csv"
   )
+
+
+  output$info <- renderText({
+    xy_str <- function(e) {
+      if(is.null(e)) return("NULL\n")
+      paste0("x=", round(e$x, 1), " y=", round(e$y, 1), "\n")
+    }
+    xy_range_str <- function(e) {
+      if(is.null(e)) return("NULL\n")
+      paste0("xmin=", round(e$xmin, 1), " xmax=", round(e$xmax, 1),
+             " ymin=", round(e$ymin, 1), " ymax=", round(e$ymax, 1))
+    }
+
+    paste0(
+      "click: ", xy_str(input$plotE_click),
+      "dblclick: ", xy_str(input$plotE_dblclick),
+      "hover: ", xy_str(input$plotE_hover),
+      "brush: ", xy_range_str(input$plotE_brush)
+    )
+  })
 
 }
 
