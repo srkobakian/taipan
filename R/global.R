49ba91a7b875b31b5fd61a37fbd1113b309ec4a0
Steff <stephanie.kobakian@gmail.com>
2018-02-07 17:24:53 +1100

    Load data files

--- R/global.R
+++ R/global.R
@@ -1,3 +1,7 @@
 
 utils::globalVariables(c("v", "question", "data", ".",
                          "path","xmin","xmax","ymin","ymax","selectionNum"))
+
+load("data/sampleQuestions.Rdata")
+load("data/tennisQuestionsSmall.Rdata")
+load("data/tennisQuestions.Rdata")
