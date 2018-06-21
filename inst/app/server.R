032ed9f1957156bbdd46037eecf0a13a2ae78e81
Mitchell <mitch.ohara-wild@monash.edu>
2018-06-21 14:18:10 +1000

    Improved skeleton of app

--- inst/app/server.R
+++ inst/app/server.R
@@ -8,6 +8,32 @@ shinyServer(
     image_list <- list.files("www/app_images", full.names = TRUE)
     output$out_img <- renderImage({
       list(src = image_list[1])
+    }, deleteFile = FALSE)
+
+    output$ui_questions <- renderUI({
+      tabBox(
+        id = "qbox",
+        width = 12,
+        tabPanel("Q1"),
+        tabPanel("Q2")
+      )
     })
+
+    output$ui_saveSelection <- renderUI({
+      if(TRUE){
+        actionLink(
+          "btn_saveSelection",
+          box(
+            "Save Selection",
+            width = 4,
+            background = "blue"
+          )
+        )
+      } else {
+        column(4)
+      }
+    })
+
+    updateTabsetPanel(session, "qbox", "Q2")
   }
 )
--- inst/app/ui.R
+++ inst/app/ui.R
@@ -12,10 +12,33 @@ shinyUI(
     dashboardBody(
       box(
         title = "Image",
-        imageOutput("out_img"),
+        imageOutput("out_img",
+                    click = clickOpts(id = "img_click"),
+                    dblclick = dblclickOpts(id = "img_dblclick"),
+                    brush = brushOpts(id = "img_brush")),
         width = 12,
         status = "primary",
         collapsible = TRUE
+      ),
+      uiOutput("ui_questions"),
+      actionLink(
+        "btn_prev",
+        box(
+          "Previous",
+          width = 3,
+          background = "green"
+        )
+      ),
+      column(1),
+      uiOutput("ui_saveSelection"),
+      column(1),
+      actionLink(
+        "btn_next",
+        box(
+          "Next",
+          width = 3,
+          background = "green"
+        )
       )
     )
   )
