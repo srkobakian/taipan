1e5c59916bbd10b039f689e4b6be3747803a1aaf
Mitchell <mitch.ohara-wild@monash.edu>
2018-06-21 13:31:27 +1000

    Start from scratch! :tada:

--- R/global.R
+++
@@ -1,6 +0,0 @@
-utils::globalVariables(c("v", "question", "data", ".",
-                         "path","xmin","xmax","ymin","ymax","selectionNum",
-                         "images", "answers", "sampleQuestions", "img_folder"))
-
-# Reactive Variables
-areaSelected <- function() NULL
