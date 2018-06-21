3d5f64d5d07ac1daca5a98a07a47398633161505
Mitchell <mitch.ohara-wild@monash.edu>
2018-06-21 13:48:28 +1000

    Improved build function

--- R/build.R
+++ R/build.R
@@ -1,4 +1,8 @@
-buildTaipan <- function(questions, images, appdir, launch = TRUE){
+buildTaipan <- function(questions, images, appdir, launch = TRUE, overwrite = FALSE){
+  # images <- tools::file_path_as_absolute(images)
+  if(overwrite){
+    unlink(appdir, recursive = TRUE)
+  }
   if(dir.exists(appdir)){
     stop(sprintf('Output appdir "%s" already exists, please provide a different location to save app',
                  appdir))
@@ -6,6 +10,7 @@ buildTaipan <- function(questions, images, appdir, launch = TRUE){
   else{
     dir.create(appdir)
   }
+  appdir <- tools::file_path_as_absolute(appdir)
 
   # WRITE APPDIR
   app_files <- list.files(system.file("app", package="taipan"))
@@ -16,10 +21,11 @@ buildTaipan <- function(questions, images, appdir, launch = TRUE){
   saveRDS(questions, file = file.path(appdir, "data", "questions.rds"))
 
   # CONSTRUCT IMAGE DIR
+  dir.create(file.path(appdir, "www", "app_images"), recursive = TRUE)
   file.copy(images, file.path(appdir, "www", "app_images", basename(images)))
 
   # LAUNCH APP
   if(launch){
-    runApp(appdir)
+    shiny::runApp(appdir)
   }
 }
